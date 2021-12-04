00b98d1c9782bee46c1fb49cb42f8e92a15d7771
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 12:25:50 +0200

    Adjusting the Clock component

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -5,14 +5,22 @@ const Clock = () => {
 
     const [time, setTime] = useState(new Date());
 
+    function refreshClock() {
+        setTime(new Date());
+      }
+
     useEffect(() => {
-        setInterval(() => {
-            setTime(new Date());
-        }, 1000);
+        
+        const timerId = setInterval(refreshClock, 1000);
+        
+        return function cleanup() {
+            clearInterval(timerId);
+          };
     }, []);
 
     return (
         <div id="clock" className="nes-container is-dark">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif" alt="Pikachu Gif" />
             <p>{time.toLocaleDateString('en-GB')}</p>
             <p>{time.toLocaleTimeString('en-GB')}</p>
         </div>

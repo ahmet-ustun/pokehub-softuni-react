37d52c8082f714c33456286f1b06e559a97fe905
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 12:26:50 +0200

    Adding the Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return ( 
+        <div id="footer" className="nes-container is-dark">
+            <p>All rights reserved</p>
+        </div>
+     );
+}
+ 
+export default Footer;

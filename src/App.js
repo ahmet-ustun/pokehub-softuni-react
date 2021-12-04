2bba1616dbcf7001e4203f152d2e116912e90c3b
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 12:27:23 +0200

    Implementing grid layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar.js';
 import Clock from './components/Clock/Clock.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
 
 	return (
-		<div className="App">
-			<Navbar />
-			<Clock />
-			<Sidebar />
-		</div>
+		<Router>
+			<div className="App">
+				<header>
+					<Navbar />
+				</header>
+				<aside>
+					<Clock />
+					<Sidebar />
+				</aside>
+				<main>
+					<Switch>
+						<Route path="/"><Home /></Route>
+					</Switch>
+				</main>
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		</Router>
 	);
 }
 

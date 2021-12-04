e4200c44f34050f754f7363de086ee5c9b534aab
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 15:56:07 +0200

    Setting up the Catch component

---
+++ src/components/Catch/Catch.js
@@ -0,0 +1,11 @@
+import './Catch.css';
+
+const Catch = () => {
+    return ( 
+        <div id="catch" className="nes-container is-dark">
+            <h2>TODO</h2>
+        </div>
+     );
+}
+ 
+export default Catch;

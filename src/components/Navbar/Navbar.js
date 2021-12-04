2070d8e75f335bc4bb184e4345289afbbf4b01d2
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 15:57:04 +0200

    Editing the links in the Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,18 @@
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const LeftSide = () => {
-    
+
     return (
         <div id="navbar-left">
-            <a href="/">
+            <Link to="/">
                 <img src="https://img.icons8.com/dusk/64/000000/home.png" alt="Home" />
                 <span>Home</span>
-            </a>
-            <a href="/catch">
+            </Link>
+            <Link to="/catch">
                 <img src="https://img.icons8.com/dusk/64/000000/open-pokeball.png" alt="Catch" />
                 <span>Catch</span>
-            </a>
+            </Link>
         </div>
     );
 }
@@ -20,18 +21,18 @@ const RightSide = () => {
 
     return (
         <div id="navbar-right">
-            <a href="/login">
+            <Link to="/login">
                 <img src="https://img.icons8.com/dusk/64/000000/login-rounded-right.png" alt="Login" />
                 <span>Login</span>
-            </a>
-            <a href="/signup">
+            </Link>
+            <Link to="/signup">
                 <img src="https://img.icons8.com/dusk/64/000000/book-and-pencil.png" alt="Signup" />
                 <span>Signup</span>
-            </a>
-            <a href="/logout">
+            </Link>
+            <Link to="/logout">
                 <img src="https://img.icons8.com/dusk/64/000000/logout-rounded-left.png" alt="Logout" />
                 <span>Logout</span>
-            </a>
+            </Link>
         </div>
     );
 }

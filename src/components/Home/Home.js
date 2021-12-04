149f828ff2570bc4413d576917f6160e1cf03fca
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 13:51:31 +0200

    Setting up the Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+    return ( 
+        <div id="home" className="nes-container is-dark">
+            
+        </div>
+     );
+}
+ 
+export default Home;

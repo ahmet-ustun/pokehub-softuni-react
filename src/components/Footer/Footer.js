386d74518d1490751c3759bdd853a855d2695d62
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-04 13:51:14 +0200

    Editing the Footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
     return ( 
         <div id="footer" className="nes-container is-dark">
-            <p>All rights reserved</p>
+            <p>Created by Ahmet Ustun</p>
         </div>
      );
 }

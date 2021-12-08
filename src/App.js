6aac77087aa7c3fe6ba80d25cd85f9d439f9ef59
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-08 21:30:34 +0200

    Setting up a default route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar.js';
 import Clock from './components/Clock/Clock.js';
-import Sidebar from './components/Sidebar/Sidebar.js';
+import { Sidebar } from './components/Sidebar/Sidebar.js';
 import Footer from './components/Footer/Footer.js';
 
 import Home from './components/Home/Home.js';
@@ -12,6 +12,7 @@ import Login from './components/Login/Login.js';
 import Signup from './components/Signup/Signup.js';
 import Pokedex from './components/Pokedex/Pokedex.js';
 import Pokemon from './components/Pokemon/Pokemon.js';
+import { NotFound2 } from './components/NotFound/NotFound.js';
 
 function App() {
 
@@ -33,6 +34,7 @@ function App() {
 						<Route exact path="/signup" component={Signup} />
 						<Route exact path="/pokedex/:letter" component={Pokedex} />
 						<Route exact path="/pokedex/:letter/:pokemon" component={Pokemon} />
+						<Route component={NotFound2} />
 					</Switch>
 				</main>
 				<footer>

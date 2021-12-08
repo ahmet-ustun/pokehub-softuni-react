87615644698fe871cbe45e0af592db3ae4991e3c
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-08 19:53:10 +0200

    Creating the NoResult component

---
+++ src/components/NoResult/NoResult.js
@@ -0,0 +1,11 @@
+import './NoResult.css';
+
+const NoResult = () => {
+    return ( 
+        <div id="no-result">
+            <img src="https://i.imgflip.com/5x4i29.jpg" alt="No result"/>
+        </div>
+     );
+}
+ 
+export default NoResult;

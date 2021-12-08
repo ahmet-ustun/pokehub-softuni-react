e2569934bf8d1a0404b9ddbb6bce38cc252d5fd9
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-08 23:59:28 +0200

    Adjusting the Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import './Footer.css';
 
 const Footer = () => {
-    return ( 
+    return (
         <div id="footer" className="nes-container is-dark">
-            <p>TODO Social Media Logos</p>
+            <span>This is a React project created by Ahmet Ustun for the SoftUni React exam.</span>
+            <div>
+                <a href="mailto:ahmetustunt@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/dusk/64/000000/gmail.png" alt="Gmail" />
+                </a>
+                <a href="https://www.linkedin.com/in/ahmet-ustun/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/dusk/64/000000/linkedin--v1.png" alt="LinkedIn" />
+                </a>
+                <a href="https://github.com/ahmet-ustun" target="_blank" rel="noopener noreferrer">
+                    <img src="https://img.icons8.com/dusk/64/000000/github.png" alt="GitHub" />
+                </a>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;

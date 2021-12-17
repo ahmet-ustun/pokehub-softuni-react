5bde1f9fdf54a94a3f252b2b864bc58a4a4f5634
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-17 19:41:26 +0200

    Adding the Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,90 +1,52 @@
 import './App.css';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { NotFound2 } from './components/NotFound/NotFound.js';
+import { AuthContext } from './contexts/Auth.js';
+import { useContext } from 'react';
 
 import Navbar from './components/Navbar/Navbar.js';
 import Clock from './components/Clock/Clock.js';
 import { Sidebar } from './components/Sidebar/Sidebar.js';
 import Footer from './components/Footer/Footer.js';
-
 import Home from './components/Home/Home.js';
+import Dashboard from './components/Dashboard/Dashboard.js';
 import Catch from './components/Catch/Catch.js';
 import Login from './components/Login/Login.js';
 import Signup from './components/Signup/Signup.js';
 import Pokedex from './components/Pokedex/Pokedex.js';
 import Pokemon from './components/Pokemon/Pokemon.js';
 
-import { NotFound2 } from './components/NotFound/NotFound.js';
-import { AuthProvider } from './contexts/Auth.js';
-import { useState, useEffect } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase.js';
-
 function App() {
 
-	const [currentUser, setCurrentUser] = useState(undefined);
+	const { currentUser } = useContext(AuthContext);
 
-    useEffect(() => {
-		
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-                setCurrentUser(user);
-            });
-
-        return unsubscribe;
-    }, []);
-
-	return currentUser ? (
-		<AuthProvider>
-			<BrowserRouter>
-				<div className="App">
-					<header>
-						<Navbar />
-					</header>
-					<aside>
-						<Clock />
-						<Sidebar />
-					</aside>
-					<main className="nes-container">
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/catch" component={Catch} />
-							<Route exact path="/pokedex/:letter" component={Pokedex} />
-							<Route exact path="/pokedex/:letter/:pokemon" component={Pokemon} />
-							<Route component={NotFound2} />
-						</Switch>
-					</main>
-					<footer>
-						<Footer />
-					</footer>
-				</div>
-			</BrowserRouter>
-		</AuthProvider>
-	) : (
-		<AuthProvider>
-			<BrowserRouter>
-				<div className="App">
-					<header>
-						<Navbar />
-					</header>
-					<aside>
-						<Clock />
-						<Sidebar />
-					</aside>
-					<main className="nes-container">
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/signup" component={Signup} />
-							<Route exact path="/pokedex/:letter" component={Pokedex} />
-							<Route exact path="/pokedex/:letter/:pokemon" component={Pokemon} />
-							<Route component={NotFound2} />
-						</Switch>
-					</main>
-					<footer>
-						<Footer />
-					</footer>
-				</div>
-			</BrowserRouter>
-		</AuthProvider>
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<header>
+					<Navbar />
+				</header>
+				<aside>
+					<Clock />
+					<Sidebar />
+				</aside>
+				<main className="nes-container">
+					<Switch>
+						<Route exact path="/" component={currentUser ? Dashboard : Home} />
+						{currentUser && <Route exact path="/catch" component={Catch} />}
+						{!currentUser && <Route exact path="/login" component={Login} />}
+						{!currentUser && <Route exact path="/signup" component={Signup} />}
+						<Route exact path="/pokedex/:letter" component={Pokedex} />
+						<Route exact path="/pokedex/:letter/:pokemon" component={Pokemon} />
+						<Route component={NotFound2} />
+					</Switch>
+				</main>
+				<footer>
+					<Footer />
+				</footer>
+			</div>
+		</BrowserRouter>
 	);
 }
 

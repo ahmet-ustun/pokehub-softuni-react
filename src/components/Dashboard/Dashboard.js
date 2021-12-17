b8bc259bfc475a84042a7922f1f78969c01d8582
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-17 19:40:28 +0200

    Adding a Dashboard component

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import './Dashboard.css';
+
+const Dashboard = () => {
+    
+    return ( 
+        <div id="dashboard" className="nes-container is-dark">
+
+        </div>
+     );
+}
+ 
+export default Dashboard;

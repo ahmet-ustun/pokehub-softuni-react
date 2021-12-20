a391d4e2be62e63a31149321be451a7c0d4d94c6
Ahmet Ustun <ahmetustunt@gmail.com>
2021-12-20 10:41:20 +0100

    Setting up the catching logic

--- src/components/Catch/Catch.js
+++ src/components/Catch/Catch.js
@@ -1,22 +1,95 @@
 import './Catch.css';
 
-const Catch1 = () => {
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../contexts/Auth.js';
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from '../../firebase.js';
+import { pokemonList } from '../../database/pokemonList.js';
 
-    return (
-        <>
-            <div class="nes-container is-rounded">
-                <img className='pokemon' src="/assets/catch/MissingNo.gif" alt="MissingNo" />
-                <img className='trainer' src="/assets/catch/Trainer.png" alt="Trainer" />
-            </div>
-            <button type="button" class="nes-btn is-success">Catch</button>
-        </>
-    );
-};
+function getRandomCode(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const Image = ({ isLoading, isFound }) => {
+
+    if (!isLoading && !isFound) {
+        return <img className='pokemon' src="/assets/catch/MissingNo.png" alt="Target" />;
+    } else if (isLoading && !isFound) {
+        return <img className='pokemon' src="/assets/catch/MissingNo.gif" alt="Catching" />;
+    } else if (!isLoading && isFound) {
+        return <img className='pokemon' src={`/assets/gif/${isFound}.gif`} alt="Caught" />;
+    }
+}
 
 const Catch = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [isFound, setIsFound] = useState(undefined);
+    const { currentUser } = useContext(AuthContext);
+
+    const today = new Date();
+    const date = `${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+    const user = currentUser.uid;
+
+    async function catchPokemon() {
+
+        try {
+            const docRef = doc(db, "users", user);
+            const docSnap = await getDoc(docRef);
+            const docData = docSnap.data();
+
+            if (docData.collection.length === 151) {
+                setIsLoading(false);
+                setIsFound(undefined);
+                alert('You have caught all the Pokémon!');
+
+            } else {
+
+                if (docData.lastCaught === date) {
+                    setIsLoading(false);
+                    setIsFound(undefined);
+                    alert('You caught a Pokémon for today!');
+
+                } else {
+                    setIsLoading(true);
+                    setIsFound(undefined);
+                    let searchCode = getRandomCode(1, 151);
+
+                    while (docData.collection.includes(searchCode)) {
+                        searchCode = getRandomCode(1, 151);
+                    };
+
+                    await setDoc(docRef, {
+                        lastCaught: date,
+                        collection: [...docData.collection, searchCode]
+                    });
+
+                    setIsLoading(false);
+                    setIsFound(searchCode);
+                    const pokemonName = pokemonList[searchCode - 1].name;
+                    alert(`Gotcha! ${pokemonName.toUpperCase()} was caught!`);
+                }
+            }
+
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
         <div id="catch" className="nes-container is-dark">
-            <Catch1 />
+            <div className="nes-container is-rounded">
+                <Image isLoading={isLoading} isFound={isFound} />
+                <img className='trainer' src="/assets/catch/Trainer.png" alt="Trainer" />
+            </div>
+            <button
+                type="button"
+                onClick={catchPokemon}
+                className="nes-btn is-success"
+                disabled={isLoading}
+            >{isLoading ? "Loading" : "Catch"}</button>
         </div>
     );
 }
